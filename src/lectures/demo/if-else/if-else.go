72161f05ed8f5361fcb746899f13c4fcba8500e0
Lars Heller <l.heller@tarent.de>
2022-10-12 09:15:26 +0200

    did the if-else demo

--- src/lectures/demo/if-else/if-else.go
+++ src/lectures/demo/if-else/if-else.go
@@ -8,5 +8,19 @@ func average(a, b, c int) float32 {
 }
 
 func main() {
+	quiz1, quiz2, quiz3 := 9, 7, 8
 
+	if quiz1 > quiz2 {
+		fmt.Println("quiz1 is greater than quiz2")
+	} else if quiz1 < quiz2 {
+		fmt.Println("quiz1 is lower than quiz2")
+	} else {
+		fmt.Println("quiz1 and quiz2 are equal")
+	}
+
+	if average(quiz1, quiz2, quiz3) > 7 {
+		fmt.Println("average greater 7")
+	} else {
+		fmt.Println("average below 7")
+	}
 }
