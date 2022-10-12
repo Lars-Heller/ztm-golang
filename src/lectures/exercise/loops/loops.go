7715d1433ec1b4817cdb9eb14d5a38479f2ac3ed
Lars Heller <l.heller@tarent.de>
2022-10-12 16:59:12 +0200

    done loops demo and exercise

--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -15,4 +15,20 @@ package main
 import "fmt"
 
 func main() {
+
+	for i := 1; i <= 50; i++ {
+		out := ""
+		if i%3 == 0 {
+			out += "Fizz"
+		}
+		if i%5 == 0 {
+			out += "Buzz"
+		}
+
+		if out == "" {
+			out = fmt.Sprint(i)
+		}
+
+		fmt.Println(out)
+	}
 }
