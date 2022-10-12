5237d125da0207abb399d37a5974dc9619010d09
Lars Heller <l.heller@tarent.de>
2022-10-12 16:29:29 +0200

    done switch demo

--- src/lectures/demo/switch/switch.go
+++ src/lectures/demo/switch/switch.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func price() int {
-	return 1
+	return 21
 }
 
 const (
@@ -13,5 +13,25 @@ const (
 )
 
 func main() {
+	switch p := price(); {
+	case p < 2:
+		fmt.Println("cheap")
+	case p < 10:
+		fmt.Println("moderate")
+	default:
+		fmt.Println("expensive")
+	}
+
+	ticket := FirstClass
+	switch ticket {
+	case Economy:
+		fmt.Println("economy")
+	case Business:
+		fmt.Println("business")
+	case FirstClass:
+		fmt.Println("first class")
+	default:
+		fmt.Println("other")
+	}
 
 }
