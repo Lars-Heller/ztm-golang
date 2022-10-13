3f6427c3a2ef9e79d3296e363285c5eaeff08f26
Lars Heller <l.heller@tarent.de>
2022-10-13 14:58:56 +0200

    done dice exercise

--- src/lectures/exercise/sr-dice/dice.go
+++ src/lectures/exercise/sr-dice/dice.go
@@ -23,7 +23,43 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func rollDie(sides int) int {
+	return rand.Intn(sides) + 1
+}
 
 func main() {
+	const NumberOfDice = 2
+	const NumberOfRolls = 3
+	const NumberOfSides = 6
+
+	rand.Seed(time.Now().UnixNano())
+
+	rollSum := 0
+	for roll := 1; roll <= NumberOfRolls; roll++ {
+		fmt.Println("doing roll", roll)
+		diceSum := 0
+		for dice := 1; dice <= NumberOfDice; dice++ {
+			rolled := rollDie(NumberOfSides)
+			fmt.Println("rolled dice", dice, "⇒", rolled)
+			diceSum += rolled
+		}
+		fmt.Println("diceSum is", diceSum)
+		switch {
+		case diceSum == 2 && NumberOfDice == 2:
+			fmt.Println("Snake Eyes")
+		case diceSum == 7:
+			fmt.Println("Lucky 7")
+		case diceSum%2 == 0:
+			fmt.Println("Even")
+		case diceSum%2 == 1:
+			fmt.Println("Odd")
+		}
+		rollSum += diceSum
+	}
 }
