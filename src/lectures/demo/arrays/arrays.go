54bc907cc341ae1eea39a39a5e73f86b19dd43be
Lars Heller <l.heller@tarent.de>
2022-10-13 17:16:51 +0200

    done arrays demo and exercise

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name    string
+	cleaned bool
+}
 
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Println(room.name, "name is clean")
+		} else {
+			fmt.Println(room.name, "is dirty")
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room{
+		{name: "Office"},
+		{name: "Warehouse"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+	checkCleanliness(rooms)
+
+	rooms[3].cleaned = true
+
+	checkCleanliness(rooms)
 }
--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -3,21 +3,50 @@
 //  out information about the list.
 //
 //--Requirements:
-//* Using an array, create a shopping list with enough room
-//  for 4 products
-//  - Products must include the price and the name
-//* Insert 3 products into the array
-//* Print to the terminal:
-//  - The last item on the list
-//  - The total number of items
-//  - The total cost of the items
-//* Add a fourth product to the list and print out the
-//  information again
 
 package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	name  string
+	price int
+}
 
+func printStats(list [4]Product) {
+	var cost, totalItems int
+	var lastItem Product
+
+	for i := 0; i < len(list); i++ {
+		item := list[i]
+		if item.name != "" {
+			cost += item.price
+			totalItems++
+			lastItem = item
+		}
+	}
+	fmt.Println("Last item on the list is", lastItem)
+	fmt.Println("cost is", cost)
+	fmt.Println(totalItems, "on the list")
+}
+
+func main() {
+	//   - Using an array, create a shopping list with enough room
+	//     for 4 products
+	var shoppingList [4]Product
+	//   - Products must include the price and the name
+	//
+	// * Insert 3 products into the array
+	shoppingList[0] = Product{"Klopapier", 289}
+	shoppingList[1] = Product{"Seife", 700}
+	shoppingList[2] = Product{"Äpfel", 349}
+	//* Print to the terminal:
+	printStats(shoppingList)
+	//  - The last item on the list
+	//  - The total number of items
+	//  - The total cost of the items
+	//* Add a fourth product to the list and print out the
+	//  information again
+	shoppingList[3] = Product{"Essig", 379}
+	printStats(shoppingList)
 }
