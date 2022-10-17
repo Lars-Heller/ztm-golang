d19ee0151687162662fad959181bf69a0ed3d5c6
Lars Heller <l.heller@tarent.de>
2022-10-17 15:15:08 +0200

    done slices demo/exercise

--- src/lectures/exercise/slices/slices.go
+++ src/lectures/exercise/slices/slices.go
@@ -18,5 +18,28 @@ import "fmt"
 
 type Part string
 
+func printAssemblyLine(title string, line []Part) {
+	fmt.Println("\n---", title, "---")
+	for i := 0; i < len(line); i++ {
+		item := line[i]
+		fmt.Println(item)
+	}
+}
+
 func main() {
+	// * Using a slice, create an assembly line that contains type Part
+	assemblyLine := []Part{}
+	// * Create a function to print out the contents of the assembly line
+	printAssemblyLine("Start", assemblyLine)
+	// * Perform the following:
+	//   - Create an assembly line having any three parts
+	assemblyLine = append(assemblyLine, "Part 1", "Part 2", "Part 3")
+	printAssemblyLine("Add 3 parts", assemblyLine)
+	//   - Add two new parts to the line
+	assemblyLine = append(assemblyLine, "Part 4", "Part 5")
+	printAssemblyLine("Add 2 parts", assemblyLine)
+	//   - Slice the assembly line so it contains only the two new parts
+	assemblyLine = assemblyLine[3:]
+	printAssemblyLine("Remove first 3 parts", assemblyLine)
+	//   - Print out the contents of the assembly line at each step
 }
