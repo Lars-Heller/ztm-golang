8c78b16a48215d29f0ee5dcee026f4fa0543d023
Lars Heller <l.heller@tarent.de>
2022-10-24 12:23:19 +0200

    done testing exercise

--- src/lectures/exercise/testing/rcv-func_test.go
+++ src/lectures/exercise/testing/rcv-func_test.go
@@ -10,3 +10,34 @@
 //
 //--Notes:
 //* Use `go test -v ./exercise/testing` to run these specific tests
+
+package main
+
+import "testing"
+
+func TestRest(t *testing.T) {
+	cases := []struct {
+		health, maxHealth, expectedHealth Health
+		energy, maxEnergy, expectedEnergy Energy
+	}{
+		{1, 2, 2, 0, 10, 10},
+		{10, 10, 10, 4, 4, 4},
+		{0, 4, 4, 0, 6, 6},
+	}
+	for _, tc := range cases {
+		p := Player{
+			name:      "Bob",
+			health:    tc.health,
+			maxHealth: tc.maxHealth,
+			energy:    tc.energy,
+			maxEnergy: tc.maxEnergy,
+		}
+		p.rest()
+		if p.health != tc.expectedHealth {
+			t.Errorf("should be max health after rest, got %v, wanted %v", p.health, tc.expectedHealth)
+		}
+		if p.energy != tc.expectedEnergy {
+			t.Errorf("should be max energy after rest, got %v, wanted %v", p.energy, tc.expectedEnergy)
+		}
+	}
+}
