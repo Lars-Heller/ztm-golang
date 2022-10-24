14e995c8de9c60dd3426c4d3a20ff4f05f9984e5
Lars Heller <l.heller@tarent.de>
2022-10-24 14:27:04 +0200

    done interace demo

--- src/lectures/demo/interfaces/interfaces.go
+++ src/lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
-func main() {
+type Preparer interface {
+	PrepareDish()
+}
 
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("cook chicken")
+}
+
+func (s Salad) PrepareDish() {
+	fmt.Println("chop salad")
+	fmt.Println("add dressing")
+}
+
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare dishes:")
+	for _, dish := range dishes {
+		fmt.Printf("--- Dish: %v ---\n", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+}
+
+func main() {
+	dishes := []Preparer{Chicken("Chicken Winds"), Salad("Caesar salad")}
+	prepareDishes(dishes)
 }
