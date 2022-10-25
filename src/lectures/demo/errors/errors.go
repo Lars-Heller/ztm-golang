b497f8b6ae373da69519275a34e48099cc1dd84a
Lars Heller <l.heller@tarent.de>
2022-10-25 16:14:10 +0200

    done errors demo

--- src/lectures/demo/errors/errors.go
+++ src/lectures/demo/errors/errors.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
-func main() {
+type Stuff struct {
+	values []int
+}
 
+func (s *Stuff) Get(index int) (int, error) {
+	if index >= len(s.values) {
+		return 0, fmt.Errorf("no element at index %v", index)
+	} else {
+		return s.values[index], nil
+	}
+}
+
+func main() {
+	stuff := Stuff{[]int{3, 2, 1}}
+	value, err := stuff.Get(3)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("value is", value)
+	}
 }
